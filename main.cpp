58ba51f821e8b0bbe096e8f77027540f174d229a
OldScripter <Oldscripter@gmail.com>
2021-07-06 23:27:09 +0300

    feature (main) add registration functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,57 @@
 #include <iostream>
+#include <map>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::map<std::string, int> registration;
+    do
+    {
+        std::cout << "Please enter the surname (or next, or exit):\n";
+        std::string surname;
+        std::cin >> surname;
+
+        if (surname == "next")
+        {
+            std::map<std::string, int>::iterator it = registration.begin();
+            if (!registration.empty())
+            {
+                std::cout << it->first << "\n";
+                if (it->second > 1)
+                {
+                    it->second--;
+                }
+                else
+                {
+                    registration.erase(it);
+                }
+            }
+            else
+            {
+                std::cerr << "Registration book is empty. Add some patients.\n";
+            }
+
+        }
+        else if (surname == "exit")
+        {
+            break;
+        }
+        else if (surname.length() == 0 || (surname.length() > 0 && surname[0] == ' '))
+        {
+            std::cerr << "Bad input. Try again.\n";
+        }
+        else
+        {
+            std::map<std::string, int>::iterator it = registration.find(surname);
+            if (it != registration.end())
+            {
+                it->second++;
+            }
+            else
+            {
+                registration.insert(std::make_pair(surname, 1));
+            }
+        }
+    } while (true);
+
+    std::cout << "Program is finished." << "\n";
     return 0;
 }
